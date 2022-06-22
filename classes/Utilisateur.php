be9a8b125526720723dcd95403ed11b636e5e3ce
Frédéric NOËL <contact@fredericnoel.com>
2022-06-22 11:29:04 +0200

    Ecriture classe Utilisateur implémentant l'interface Utilisateur

---
+++ classes/Utilisateur.php
@@ -0,0 +1,29 @@
+<?php
+
+class Utilisateur implements Utilisateur
+{
+    public function inscrireUtilisateur()
+    {
+
+    }
+    
+    public function modifierUtilisateur()
+    {
+
+    }
+
+    public function supprimerUtilisateur()
+    {
+
+    }
+
+    public function connecterUtilisateur()
+    {
+
+    }
+
+    public function deconnecterUtilisateur()
+    {
+        
+    }
+}
